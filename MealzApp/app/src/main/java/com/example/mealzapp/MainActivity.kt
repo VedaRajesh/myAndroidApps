cbbbd4415aa63f7d7e30cbee41f3229c8142d2b9
Admin <rohanveda1234@gmail.com>
2021-10-31 05:02:53 +0000

    Android Learner Banner

---
+++ MealzApp/app/src/main/java/com/example/mealzapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mealzapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mealzapp.ui.theme.MealzAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text(
+                text = "Hello World!",
+                fontSize = 30.sp
+            )
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Text(
+        text = "Hello World!",
+        fontSize = 30.sp
+    )
+}
