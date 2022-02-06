dc8504e715265b04e2df125fe0d1f0de4b49da0d
Rohan.R <rohanveda1234@gmail.com>
2022-02-06 02:38:45 -0500

    it is the project of jetpack comose room database in kotlin

---
+++ JCRoomDatabase/app/src/main/java/com/example/jcroomdatabase/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.jcroomdatabase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jcroomdatabase.ui.theme.JCRoomDatabaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JCRoomDatabaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JCRoomDatabaseTheme {
+
+    }
+}
